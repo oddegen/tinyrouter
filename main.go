d15b1a5325d14bc9bee1c54d1b19a75510adfae7
oddegen <nahomtensae@gmail.com>
2024-03-06 12:18:56 +0300

    fix bugs

--- main.go
+++ main.go
@@ -7,34 +7,36 @@ import (
 )
 
 func main() {
+	tinyrouter.RedirectTrailingSlash = true
+
 	router := tinyrouter.NewRouter()
 
-	router.Handle(http.MethodGet, "/movie/:id/genre/action", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc(http.MethodGet, "/movie/:id/genre/action", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("GET :8000 /movie/:id GOT MOVIE WITH ID: " + router.GetParam(r, "id")))
 	})
 
-	router.Handle(http.MethodPost, "/movie", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc(http.MethodPost, "/movie", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("POST :8000 INSERTED MOVIE"))
 	})
 
-	router.Route("/articles", func(tr *tinyrouter.Router) {
+	router.Group("/articles", func(tr *tinyrouter.Router) {
 
-		tr.Handle(http.MethodPost, "/*wild", func(w http.ResponseWriter, r *http.Request) {
+		tr.HandleFunc(http.MethodPost, "/*wild", func(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte("POST :8000 INSERTED ARTICLE" + tr.GetParam(r, "wild")))
 		})
 
-		tr.Handle("http.MethodGet", "/search", func(w http.ResponseWriter, r *http.Request) {
+		tr.HandleFunc("http.MethodGet", "/search", func(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte("GET :8000 SEARCHING ARTICLES"))
 		})
 
-		tr.Route("/:articlesId", func(tr *tinyrouter.Router) {
-			tr.Handle(http.MethodGet, "/", func(w http.ResponseWriter, r *http.Request) {
+		tr.Group("/:articlesId", func(tr *tinyrouter.Router) {
+			tr.HandleFunc(http.MethodGet, "/", func(w http.ResponseWriter, r *http.Request) {
 				w.Write([]byte("GET :8000 SEARCHING ARTICLES"))
 			})
-			tr.Handle(http.MethodPut, "/", func(w http.ResponseWriter, r *http.Request) {
+			tr.HandleFunc(http.MethodPut, "/", func(w http.ResponseWriter, r *http.Request) {
 				w.Write([]byte("PUT :8000 UPDATING ARTICLES"))
 			})
-			tr.Handle(http.MethodDelete, "/", func(w http.ResponseWriter, r *http.Request) {
+			tr.HandleFunc(http.MethodDelete, "/", func(w http.ResponseWriter, r *http.Request) {
 				w.Write([]byte("DELETE :8000 DELETING ARTICLES"))
 			})
 		})
--- pkg/tinyrouter.go
+++ pkg/tinyrouter.go
@@ -7,56 +7,103 @@ import (
 	"strings"
 )
 
-// pattern: /movies/:id => /movies/12 || /movies/one
-// pattern: /movies/* => /movies/two || /movies/category/action
+var (
+	RedirectTrailingSlash bool
+)
 
 type Router struct {
-	routes map[string]route
+	prefix string
+	routes []*route
+}
+
+type middleware func(http.Handler) http.Handler
+
+func Use(middlewares ...middleware) middleware {
+	return func(h http.Handler) http.Handler {
+		for i := range middlewares {
+			h = middlewares[len(middlewares)-1-i](h)
+		}
+
+		return h
+	}
+}
+
+type route struct {
+	method  string
+	pattern string
+	handler http.Handler
 }
 
 func NewRouter() *Router {
 	return &Router{
-		routes: make(map[string]route),
+		routes: make([]*route, 0),
 	}
 }
 
-type ctxKey string
+func (router *Router) Handle(method string, pattern string, handler http.Handler) {
 
-type route struct {
-	handler http.HandlerFunc
-	method  string
-	pattern string
-}
+	if method == "" {
+		panic("tinyrouter: error no http method")
+	}
 
-func (r *Router) Handle(method, pattern string, handler http.HandlerFunc) {
-	pattern = cleanPath(pattern)
+	if handler == nil {
+		panic("tinyrouter: error no handler")
+	}
 
-	route := route{
-		method:  strings.ToUpper(method),
+	validatePattern(pattern)
+
+	if router.prefix != "" {
+		pattern = path.Join(router.prefix, pattern)
+	}
+
+	route := &route{
+		method:  method,
 		pattern: pattern,
 		handler: handler,
 	}
 
-	r.routes[pattern+method] = route
+	router.routes = append(router.routes, route)
+
 }
 
-var params = make(map[string]string)
+func (router *Router) HandleFunc(method string, pattern string, handler http.HandlerFunc) {
+	router.Handle(method, pattern, handler)
+}
+
+func (r *Router) Group(pattern string, fn func(r *Router)) {
+	subrouter := NewRouter()
+	subrouter.prefix = path.Join(r.prefix, pattern)
+
+	fn(subrouter)
+
+	r.routes = append(r.routes, subrouter.routes...)
+}
+
+type ctxKey string
 
 func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
 	allows := make([]string, 0)
 
 	for _, route := range router.routes {
+		path := cleanPath(r.URL.Path)
 
-		if match(route.pattern, cleanPath(r.URL.Path)) {
+		if ok, trail := match(route.pattern, path); ok {
 			if r.Method != route.method {
 				allows = append(allows, route.method)
 				continue
 			}
-			ctx := context.WithValue(r.Context(), ctxKey("ctxKey"), params)
-			route.handler(w, r.WithContext(ctx))
-			return
-		}
 
+			if trail && RedirectTrailingSlash {
+				http.Redirect(w, r, removeTrailingSlash(path), http.StatusSeeOther)
+				return
+			} else if !trail {
+				params := parseParams(route.pattern, path)
+				ctx := context.WithValue(r.Context(), ctxKey("ctxKey"), params)
+				route.handler.ServeHTTP(w, r.WithContext(ctx))
+				return
+			}
+		}
 	}
 
 	if len(allows) > 0 {
@@ -66,7 +113,6 @@ func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.NotFound(w, r)
-
 }
 
 func (route *Router) GetParam(r *http.Request, key string) string {
@@ -74,64 +120,95 @@ func (route *Router) GetParam(r *http.Request, key string) string {
 	return field[key]
 }
 
-func (r *Router) Group(pattern string, fn func(r *Router)) {
-	subrouter := NewRouter()
+func match(pattern, path string) (match, trail bool) {
+	key := make(map[string]struct{})
 
-	fn(subrouter)
-	for _, route := range subrouter.routes {
-		newPath := pattern + route.pattern
-		r.Handle(route.method, newPath, route.handler)
+	if path[len(path)-1] == '/' {
+		path = removeTrailingSlash(path)
+		trail = true
 	}
-}
 
-func match(pattern, path string) bool {
-
-	for i := 1; len(path) != 0 && len(pattern) != 0; pattern = pattern[i:] {
+	for ; len(path) != 0 && len(pattern) != 0; pattern = pattern[1:] {
 		switch pattern[0] {
-		case '*':
-			if len(pattern) < 2 {
-				panic("tinyrouter: Error no identifier for wildcard param")
+		case '*', ':':
+			param := getParam(pattern[1:])
+			if _, ok := key[param]; ok {
+				panic("tinyrouter: error wild-characters with identical names")
+			}
+			key[param] = struct{}{}
+
+			if pattern[0] == '*' {
+				path = path[len(path):]
+			} else {
+				path = path[len(getParam(path)):]
 			}
 
-			i = len(pattern)
-			params[pattern[1:]] = path
-			path = path[len(path):]
-		case ':':
-			i = getParam(pattern)
-
-			if len(pattern[:i]) < 2 {
-				panic("tinyrouter: Error no identifier for named param")
-			}
-
-			if pattern[1] == '*' {
-				panic("tinyrouter: Error can't use wildcard (*) after a named param")
-			}
-
-			if len(path) == 0 {
-				continue
-			}
-
-			params[pattern[1:i]] = path[:getParam(path)]
-
-			path = path[getParam(path):]
+			pattern = pattern[len(param):]
 		case path[0]:
-			i = 1
 			path = path[1:]
 		default:
-			return false
+			return false, trail
 		}
 	}
-	return len(pattern) == 0 && len(path) == 0
+	return len(pattern) == 0 && len(path) == 0, trail
 }
 
-func getParam(pattern string) int {
-	i := strings.IndexByte(pattern, '/')
+func parseParams(pattern, path string) (params map[string]string) {
+	params = make(map[string]string)
 
-	if i < 0 {
-		i = len(pattern)
+	for ; len(path) != 0 && len(pattern) != 0; pattern = pattern[1:] {
+		switch pattern[0] {
+		case '*':
+			k, v := getParam((pattern)[1:]), path
+			params[k] = v
+		case ':':
+			k, v := getParam((pattern)[1:]), getParam(path)
+			params[k] = v
+		}
+		path = path[1:]
 	}
 
-	return i
+	return
+}
+
+func getParam(p string) string {
+	index := strings.Index(p, "/")
+
+	if index < 0 {
+		index = len(p)
+	}
+
+	return p[:index]
+}
+
+func validatePattern(pattern string) {
+
+	if len(pattern) < 1 {
+		panic("tinyrouter: error no given pattern")
+	}
+
+	pattern = removeTrailingSlash(pattern)
+
+	for i, c := range []byte(pattern) {
+		if c != '*' && c != ':' {
+			continue
+		}
+
+		if len(pattern[i+1:]) == 0 || pattern[i+1] == '/' {
+			panic("tinyrouter: error un-named wildcard characters")
+		}
+
+		if c == '*' && strings.Contains(pattern[i:], "/") || c == '*' && strings.Contains(pattern[i:], ":") {
+			panic("tinyrouter: error incorrect use of catch-all(*) wildcard matcher")
+		}
+
+		if c == ':' {
+			if slash := strings.Index(pattern[i:], "/"); slash != -1 && strings.Contains(pattern[i+1:i+slash], "*") {
+				panic("tinyrouter: error incorrect use of param(:) wildcard matcher")
+			}
+		}
+	}
+
 }
 
 func cleanPath(p string) string {
@@ -153,4 +230,11 @@ func cleanPath(p string) string {
 		}
 	}
 	return np
+}
+
+func removeTrailingSlash(path string) string {
+	if path[len(path)-1:] == "/" {
+		path = path[:len(path)-1]
+	}
+	return path
 }
